3a38657fc099fdbb296e2dbd5c84d89f1def95cf
evan.yjz <evan.yjz@gmail.com>
2018-02-05 18:43:45 +0800

    yjz

---
+++ src/js/pages/btscan/home/home.vue
@@ -0,0 +1,31 @@
+<template>
+    <text class="title">室内设备管理</text>
+    <div>
+
+    </div>
+
+</template>
+
+<script>
+    import "Config";
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+
+        mounted() {
+
+        },
+
+        method: {
+
+        }
+    }
+
+
+</script>
+
+<style lang="sass" src="./home.scss" scoped></style>
