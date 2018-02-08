d451f0fefef19c56bdc15627ab81a473dd5b8898
evan.yjz <evan.yjz@gmail.com>
2018-02-08 20:43:24 +0800

    yjz

--- src/js/pages/btscan/history-records/innerPage/detail-list.vue
+++ src/js/pages/btscan/history-records/innerPage/detail-list.vue
@@ -1,23 +1,23 @@
 <template>
     <div>
-        <text>{{}}</text>
-
+        <div class="top-box">
+            <text class="task-name">{{taskName}}</text>
+        </div>
         <div class="list-head">
             <text class="left-text">设备标签</text>
             <text class="middle-text">设备名称</text>
             <text class="right-text">电量</text>
         </div>
 
-        <!--<list class="list">-->
-            <!--<cell v-for="(item, index) in tasks" :key="index" @click="goTo(item)">-->
-                <!--<div class="cellInnerDiv" :style="{backgroundColor: (index % 2) ? '#90D8FF' : 'white'}">-->
-                    <!--<text class="common-style text-taskName">{{item.taskName}}</text>-->
-                    <!--<text class="common-style text-time">{{item.time}}</text>-->
-                    <!--<text class="common-style text-devNum" >{{item.devNum}}</text>-->
-                    <!--<text class="common-style copy-icon" @click="copyDev(item)">&#xe85e;</text>-->
-                <!--</div>-->
-            <!--</cell>-->
-        <!--</list>-->
+        <list class="list">
+            <cell v-for="(item, key, index) in devices" :key="index">
+                <div class="cellInnerDiv" :style="{backgroundColor: (index % 2) ? '#90D8FF' : 'white'}">
+                    <text class="common-style text-key">{{key}}</text>
+                    <text class="common-style text-alias">{{item.alias}}</text>
+                    <text class="common-style text-battery" >{{item.battery}}</text>
+                </div>
+            </cell>
+        </list>
     </div>
 
 </template>
@@ -25,11 +25,11 @@
 <script>
     if (process.env.NODE_ENV === 'development') require('Config');
 
-
     export default {
         data() {
             return {
-                task: {}
+                taskName: '',
+                devices: {}
             }
         },
 
@@ -39,7 +39,8 @@
 
         bmRouter: {
             viewDidAppear(params) {
-                this.task = params
+                this.taskName = params.taskName;
+                this.devices = params.devices;
             }
         },
 
