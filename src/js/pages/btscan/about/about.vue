e64d33c2b0ab31eb98b5bc97decfc5d134d157c9
evan.yjz <evan.yjz@gmail.com>
2018-02-11 18:09:02 +0800

    yjz

--- src/js/pages/btscan/about/about.vue
+++ src/js/pages/btscan/about/about.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
+    <div class="box">
+        <div class="text-box">
+            <text class="text-normal">appVersion:  </text>
+            <text class="text-normal">1.0.0</text>
+        </div>
+        <div class="text-box">
+            <text class="text-normal">designBy:  </text>
+            <text class="text-normal">叶杰中</text>
+        </div>
+        <div class="text-box">
+            <text class="text-normal">email:  </text>
+            <text class="text-normal">evan.yjz@gmail.com</text>
+        </div>
     </div>
-
 </template>
 
 <script>
